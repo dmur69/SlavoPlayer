bae8b718fb1c5de60387524500aec0cf9eae763c
Dennis Murnikow <murnikow@innokraft.de>
2021-11-07 15:36:20 +0100

    New responcive TrackPlayer integrated into Track.vue

--- src/components/track/TrackPlayer.vue
+++ src/components/track/TrackPlayer.vue
@@ -1,91 +1,78 @@
 <template>
-  <section class="bg-white pb-4 align-top w-full h-30">
-    <div class="relative">
-      <div class="flex justify-center">
-        <!-- Buttons -->
-        <div class="flex text-gray-500 text-3xl m-5">
-          <!-- Play/Pause -->
-          <button
-            id="play-next"
-            v-if="currentPlaylist.seek"
-            type="button"
-            @click.prevent="playPrevTrack"
-          >
-            <i class="fa fa-fast-backward" />
-          </button>
-          <!-- Back Button -->
-          <button id="play-player" type="button" @click.prevent="togglePlaying">
-            <i
-              class="fa px-5"
-              :class="{
-                'fa-play': !trackIsPlaying,
-                'fa-pause': trackIsPlaying
-              }"
-            />
-          </button>
-          <!-- Next Button -->
-          <button
-            id="play-next"
-            v-if="currentPlaylist.seek"
-            type="button"
-            @click.prevent="playNextTrack"
-          >
-            <i class="fa fa-fast-forward" />
-          </button>
-        </div>
-        <!-- Seek ticker for mobile-->
-        <div class="md:hidden text-gray-500 font-bold text-xl pt-6">
-          <span class="player-currenttime">{{ currentTrack.seek }}</span>
-        </div>
-      </div>
-      <!-- Progress and duration -->
-      <div class="md:flex text-xl">
-        <!-- Seek ticker for desktop -->
-        <div class="hidden md:block text-gray-500 pl-5">
-          <span class="player-currenttime">{{ currentTrack.seek }}</span>
-        </div>
-        <!-- Progress bar -->
-        <div class="w-full pt-3 pl-5 player-scrub">
-          <!-- Progress: Container  -->
-          <!--<<<< HIER we have to listen on Klick-events for track positioning   >>>>-->
-          <span
-            class="block w-full rounded bg-gray-200 relative cursor-pointer"
-            @click.prevent="updateSeek"
-          >
-            <!-- Player Ball -->
-            <span
-              class="absolute top-neg-8 text-gray-800 text-lg"
-              :style="{ left: currentTrack.seekPercentage }"
-            >
-              <i class="fas fa-circle" />
-            </span>
-            <!-- Player Progress Bar-->
-            <span
-              class="
-                block
-                h-2
-                rounded
-                bg-gradient-to-r
-                from-green-500
-                to-green-400
-              "
-              :style="{ width: currentTrack.seekPercentage }"
-            />
-          </span>
-        </div>
-        <!-- Duration -->
-        <div class="hidden md:block text-gray-500 px-3">
-          <span class="player-duration">{{ currentTrack.duration }}</span>
-        </div>
-      </div>
-      <!-- Track title -->
-      <div class="text-lg text-center mt-3">
-        <span class="song-title">{{ currentTrack.meta.title }}</span>
-        <span v-if="currentTrack.sound.playing"> | </span>
-        <span class="song-artist">Глава {{ currentTrack.meta.chapter }}</span>
-      </div>
+  <div class="flex justify-center">
+    <!-- Buttons -->
+    <div class="flex text-gray-300 text-3xl m-5 space-x-5">
+      <!-- Play/Pause -->
+      <button
+        id="play-next"
+        v-if="currentPlaylist.seek"
+        type="button"
+        @click.prevent="playPrevTrack"
+      >
+        <i class="fa fa-fast-backward" />
+      </button>
+      <!-- Back Button -->
+      <button id="play-player" type="button" @click.prevent="togglePlaying">
+        <i
+          class="fa px-5"
+          :class="{
+            'fa-play': !trackIsPlaying,
+            'fa-pause': trackIsPlaying
+          }"
+        />
+      </button>
+      <!-- Next Button -->
+      <button
+        id="play-next"
+        v-if="currentPlaylist.seek"
+        type="button"
+        @click.prevent="playNextTrack"
+      >
+        <i class="fa fa-fast-forward" />
+      </button>
     </div>
-  </section>
+    <!-- Seek ticker for mobile-->
+    <div class="md:hidden text-gray-300 font-bold text-xl pt-6">
+      <span class="player-currenttime">{{ currentTrack.seek }}</span>
+    </div>
+  </div>
+  <!-- Progress and duration -->
+  <div class="md:flex text-xl justify-center">
+    <!-- Seek ticker for desktop -->
+    <div class="hidden md:block text-gray-300 pl-5">
+      <span class="player-currenttime">{{ currentTrack.seek }}</span>
+    </div>
+    <!-- Progress bar -->
+    <div class="w-full pt-3 pl-5 player-scrub">
+      <!-- Progress: Container  -->
+      <!--<<<< HIER we have to listen on Klick-events for track positioning   >>>>-->
+      <span
+        class="block w-full rounded bg-gray-500 relative cursor-pointer"
+        @click.prevent="updateSeek"
+      >
+        <!-- Player Ball -->
+        <span
+          class="absolute top-neg-8 text-gray-300 text-lg"
+          :style="{ left: currentTrack.seekPercentage }"
+        >
+          <i class="fas fa-circle" />
+        </span>
+        <!-- Player Progress Bar-->
+        <span
+          class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
+          :style="{ width: currentTrack.seekPercentage }"
+        />
+      </span>
+    </div>
+    <!-- Duration -->
+    <div class="hidden md:block text-gray-300 px-3">
+      <span class="player-duration">{{ currentTrack.duration }}</span>
+    </div>
+  </div>
+  <!-- ToDo Idea: show some status info while playing -->
+  <div class="text-lg text-center mt-3">
+    <span v-if="currentTrack.sound.playing"> </span>
+  </div>
 </template>
 
 <script>
