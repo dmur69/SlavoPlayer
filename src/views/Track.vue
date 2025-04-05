881e7b75dbcb5d8c6ee100e2773c4c540e263e10
Dennis Murnikow <murnikow@outlook.com>
2025-04-05 19:54:34 +0200

    Base removed

--- src/views/Track.vue
+++ src/views/Track.vue
@@ -13,7 +13,7 @@
       container
       items-center
     "
-    style="background-image: url('/SlavoPlayer/song-header.png')"
+    style="background-image: url('/song-header.png')"
   >
     <div class="lg:flex">
       <!-- Player -->
